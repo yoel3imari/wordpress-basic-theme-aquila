900366463233c7b9eb760a61dead63ee4fdcb814
yoel3imari <youssef.elaimari@gmail.com>
2022-08-15 23:07:34 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/** 
+ * Main template file.
+ * 
+ * @package Aquila
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aquila - wordpress theme</title>
+</head>
+<body>
+    <h1>Welcome to Aquila</h1>
+</body>
+</html>
