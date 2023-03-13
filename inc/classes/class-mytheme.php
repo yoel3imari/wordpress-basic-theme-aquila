1a973d72e9adbf3188956a4ff2d130846a4b1d7a
yoel3imari <youssef.elaimari@gmail.com>
2023-03-13 22:21:26 +0100

    theme supprt options

--- inc/classes/class-mytheme.php
+++ inc/classes/class-mytheme.php
@@ -24,4 +24,21 @@ class MyTheme
     {
         //actions and filters
     }
+
+    protected function setup_theme()
+    {
+        // theme options
+        add_theme_support('title-tag');
+        add_theme_support('custom-logo', [
+            'header-text' => ['site-title', 'site-description'],
+            'height' => 100,
+            'width' => 400,
+            'flex-height' => true,
+            'flex-width' => true
+        ]);
+        add_theme_support( 'custom-background', [
+            'default-color' => '#fff',
+            'default-img' => '', 
+        ] );
+    }
 }
